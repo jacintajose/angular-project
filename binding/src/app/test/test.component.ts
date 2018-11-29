6f38580da1cf9598bd4f0db0815e414a1c3ab6d5
Jacinta Jose <jacintajose1234@gmail.com>
2018-11-29 14:09:36 -0800

    siteurl

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -9,16 +9,21 @@ import { Component, OnInit } from '@angular/core';
              </h2>
              <h2>{{2+2}}</h2>
              <h2>{{"welcome  "+ name}}
-             <h2>{{name.length}}
+             <h2>{{name.length}}</h2>
              <h2>{{name.toUpperCase()}}</h2>
+             <h2>{{greetuser()}}</h2>
+             <h2>{{siteurl}}</h2>
              `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
   constructor() { }
-  public name="Jacinta"
+  public name="Jacinta";
+  public siteurl=window.location.href;
   ngOnInit() {
   }
-
+greetuser(){
+  return "Hello"+this.name;
+}
 }
