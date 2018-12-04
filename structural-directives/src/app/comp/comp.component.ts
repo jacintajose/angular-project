90d0e06a7bce8b423309c05fb72790f4816ae4e0
Jacinta Jose <jacintajose1234@gmail.com>
2018-12-04 13:50:33 -0800

    ngSwitch

--- structural-directives/src/app/comp/comp.component.ts
+++ structural-directives/src/app/comp/comp.component.ts
@@ -10,26 +10,36 @@ import { Component, OnInit } from '@angular/core';
   <h2>
   name is hidden
   </h2>
+  </ng-template>
 
-<div *ngIf=display; then thenblock; else elseblock">
+<div *ngIf="display; then thenblock; else elsblock"></div>
   <ng-template #thenblock>
   <h2>
   HELLO
   </h2>
   </ng-template>
 
-  <ng-template #elseblock>
+  <ng-template #elsblock>
   <h2>
  HIDDEN
   </h2>
   </ng-template>
+
+  <div [ngSwitch]="color">
+  <div *ngSwitchCase ="'red'">You picked red color</div>
+  <div *ngSwitchCase ="'blue'">You picked blue color</div>
+  <div *ngSwitchCase ="'green'">You picked green color</div>
+  <div *ngSwitchDefault>Pick Again</div>
+
+<div>
 
   `,
   styles: []
 })
 export class CompComponent implements OnInit {
 displayname=false;
-display=false;
+display=true;
+public color="red";
   constructor() { }
 
   ngOnInit() {
