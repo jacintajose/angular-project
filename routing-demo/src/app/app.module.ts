5d5ea2cde452eee15cc4647ff713547dd1058cfc
Jacinta Jose <jacintajose1234@gmail.com>
2018-12-07 17:51:46 -0800

    routing-** no output

---
+++ routing-demo/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule, routingComponenets } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   routingComponenets
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    routingComponenets
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
